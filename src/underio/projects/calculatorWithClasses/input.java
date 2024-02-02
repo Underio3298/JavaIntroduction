146cd33d848c6ff7cccdfebcadde1dbe573c3e85
Aaron Avdeev <117237424+Underio3298@users.noreply.github.com>
2024-02-02 16:49:24 +0000

    typo fix

--- src/underio/projects/calculatorWithClasses/input.java
+++ src/underio/projects/calculatorWithClasses/input.java
@@ -12,7 +12,7 @@ public class input {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the first number:");
         num1 = scanner.nextInt();
-        System.out.println("Enter the operation you would like to use: (x, /, *, -)");
+        System.out.println("Enter the operation you would like to use: (*, /, +, -)");
         scanner.nextLine();
         operator.type = scanner.nextLine();
         System.out.println("Enter the second number:");
