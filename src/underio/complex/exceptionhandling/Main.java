0a1e9104f55bd3e77f5622378485cdaa9c2bed06
Underio3298 <underio3298@gmail.com>
2024-02-03 08:39:38 -0500

    learning about exception handling and try/catch

---
+++ src/underio/complex/exceptionhandling/Main.java
@@ -0,0 +1,21 @@
+package underio.complex.exceptionhandling;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        /* Exceptions & Try And Catch */
+
+        int x = 10;
+        try {
+            x = 100 / 0;
+        } catch (Exception e) {
+            System.out.println("Exception Caught! " + e.getMessage());
+        }
+
+        System.out.println("something important");
+
+        if (x < 100) {
+            throw new Exception("some piece oof code is broke");
+        }
+
+    }
+}
