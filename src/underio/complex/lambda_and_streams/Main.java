2a51afff228a2c0928e92b7e9dc2e7ecbc24ac0b
Underio3298 <underio3298@gmail.com>
2024-02-03 20:20:46 -0500

    learning about lambda and streams

--- src/underio/complex/lambda_and_streams/Main.java
+++ src/underio/complex/lambda_and_streams/Main.java
@@ -1,5 +1,7 @@
 package underio.complex.lambda_and_streams;
 
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Main {
@@ -12,13 +14,33 @@ public class Main {
         somethingCool(Main::helloWorld);
 
         Supplier<Float> pi = () -> 3.14159f;
+
+        Consumer<Runnable> test = runnable1 -> {
+            runnable1.run();
+        };
+        test.accept(runnable);
+
+        Runnable method = Main::helloWorld;
+
+        /* STREAMS */
+
+        List<String> fruits = List.of("Apple", "Banana", "Orange", "Mango", "Pineapple", "Mandarin");
+
+        fruits.stream()
+                .filter(fruit -> fruit.startsWith("M"))
+                .forEach(System.out::println);
     }
 
-    public static void helloWorld(){
+    public static void helloWorld() {
         System.out.println("Hello World!");
     }
 
     public static void somethingCool(Runnable runnable) {
         runnable.run();
     }
+
+    public static float getPi() {
+        return 3.14159f;
+    }
+
 }
